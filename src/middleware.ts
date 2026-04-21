3ce49dcc932250fe34776575124b51decd92b2cb
M.Fathin Halim <halimfathin7@gmail.com>
2026-04-22 06:04:50 +0700

    create middleware.ts (note: aku baru tahu ada beginian. Selama ini aku bikin satu-satu itu jelek banget. Setelah project ini kelar aku ingin ngubah project lama ku supaya make middleware.ts semua

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import { jwtVerify } from "jose";
+
+export async function middleware(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+  const { pathname } = req.nextUrl;
+
+  if (!token) {
+    if (pathname === "/login") return NextResponse.next();
+    return NextResponse.redirect(new URL("/login", "req.url"));
+  }
+
+  try {
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET_KEY || "");
+    const { payload } = await jwtVerify(token, secret);
+
+    if (payload.isFirstLogin && pathname !== "/change_password") {
+      return NextResponse.redirect(new URL("change_password", req.url));
+    }
+
+    return NextResponse.next();
+  } catch {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+}
+
+export const config = {
+  matcher: ["/((?!api|_next|favicon.ico).*)"],
+};
